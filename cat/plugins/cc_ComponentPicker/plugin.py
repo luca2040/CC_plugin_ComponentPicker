dca5381eb9145bf5ed0addb5088c93606f001435
Luca2040 <152313871+Luca2040@users.noreply.github.com>
2024-12-26 17:04:13 +0100

    Added cat files

---
+++ cat/plugins/cc_ComponentPicker/plugin.py
@@ -0,0 +1,17 @@
+from cat.mad_hatter.decorators import tool
+from cat.mad_hatter.decorators import hook
+
+
+@hook
+def agent_prompt_prefix(prefix, cat):
+
+    prefix = """"""
+
+    return prefix
+
+
+@tool
+def component_info(input, cat):
+    """"""
+
+    return ""
