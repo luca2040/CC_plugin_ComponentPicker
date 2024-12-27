1cdc9a3d40ed98933863f3eb769aa758b926d513
Luca2040 <152313871+Luca2040@users.noreply.github.com>
2024-12-27 16:50:02 +0100

    first test

--- cat/plugins/cc_ComponentPicker/functions.py
+++ cat/plugins/cc_ComponentPicker/functions.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 base_URL = "http://json-server:3000/components/"
 
@@ -11,13 +12,19 @@ def API_request(subfolder):
         (data, if list): returns the
     """
 
+    # inefficient API request just for example
+
     try:
-        print(base_URL + "/".join(subfolder))
-        response = requests.get(base_URL + "/".join(subfolder))
+        response = requests.get(base_URL)
         response.raise_for_status()
         json_data = response.json()
 
+        for folder in subfolder:
+            json_data = json_data[folder]
+
         is_list = "components" in json_data
+        if is_list:
+            json_data.pop("components")
 
         return json_data if is_list else list(json_data.keys()), is_list
 
@@ -26,13 +33,53 @@ def API_request(subfolder):
         return None
 
 
-def pick_component(query, cat):
-    print("##################")
-    response = API_request(["Active", "Diodes", "Rectifier_Diodes"])
-    if not response:
-        return "API ERROR"
+def look_into_categories(path: list, query_component, cat):
+    categories = API_request(path)
+    if not categories:
+        return []
 
-    print(response)
-    print("##################")
+    if categories[1]:
+        return [str(categories[0])]
 
-    return "component"
+    cat_query = f"""You are an electronics component picker, and you need to classify some component requests in the correct category.
+From the component characteristics and type, and given the categories, choose the categories that include the component itself.
+
+If the component can be inside more than one category, select also that categories.
+If in the description of the component are specified more components, select all the relative categories, EXAMPLE: component= {{transistor:{{type:"NPN or PNP"}}}} -> in this case it should be classified as NPN AND PNP.
+If the component is missing a detail to classify it, respond with ["#","@DETAIL@"], where @DETAIL@ is what's missing from the description, for example @MAX_CURRENT@.
+
+Your response MUST be an UNFORMATTED PURE JSON list, containing strings representing exactly the category names.
+
+REQUESTED COMPONENT:
+{query_component}
+
+CATEGORIES:
+{categories[0]}"""
+
+    cat_categories = json.loads(cat.llm(cat_query))
+
+    return_result = []
+
+    if "#" in cat_categories:
+        return cat_categories
+
+    for category in cat_categories:
+        new_path = path + [category]
+
+        new_categories = look_into_categories(new_path, query_component, cat)
+        if "#" in new_categories:
+            return new_categories
+
+        return_result += new_categories
+
+    return return_result
+
+
+def pick_component(query_component, cat):
+
+    components = look_into_categories([], query_component, cat)
+
+    if "#" in components:
+        return components[1], False
+    else:
+        return "\n-".join(components), True
--- cat/plugins/cc_ComponentPicker/plugin.py
+++ cat/plugins/cc_ComponentPicker/plugin.py
@@ -11,14 +11,19 @@ def agent_prompt_prefix(prefix, cat):
 
     prefix = """You are an electronics component picker AI tool.
 You help who asks to find a component that satisfies the request.
-You MUST respond only with information that was given to you explicitely and if you don't have that information say that you "don't have informations" """
+You MUST respond only with information that was given to you explicitely and if you don't have that information say that you "don't have informations".
+
+**IMPORTANT**
+If a TOOL OUTPUT returns the character # followed by @request@, it means that the user didn't specify the @request@ component characteristisc, so you MUST respond to tell the user to INCLUDE THAT SPECIFIC INFO.
+
+ALWAYS USE CHAT CONTEXT TO CONTINUE THE DIALOGUE AND USE TOOLS, IF SPECIFIED IN OTHER MESSAGES."""
 
     return prefix
 
 
 @tool
 def component_info(input, cat):
-    """Use this tool to identify and structure necessary information from user questions about electronic components.  
+    """Use this tool to identify and structure necessary information from user questions about electronic components, or continuation from previous dialogs and messages.  
 
 **Input Format**: The input should be a JSON list, where each component is an object with required parameters extracted from the user's query. Each component is named with a unique code (e.g., "C1" for the first capacitor or "IC2" for the second IC).  
 
@@ -51,6 +56,18 @@ Tool Input:
 
     components = json.loads(input)
 
-    results = [pick_component(component, cat) for component in components]
+    results = ""
+    
+    
 
-    return "\n".join(results)
+    for component in components:
+        picked_component, found = pick_component(component, cat)
+
+        if found:
+            results += f"""\n\n\nFound components: {
+                picked_component}\n\n for request: {str(component)}"""
+        else:
+            results += f"""\n\n\nNOT fount: {str(component)}\n\n Because the detail {
+                picked_component} is missing."""
+
+    return results
